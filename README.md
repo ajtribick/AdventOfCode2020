b951718de501d640a04846946b7918c90e1b9435
Andrew Tribick <ajtribick@googlemail.com>
2020-12-02 17:47:12 +0100

    Day 2, part 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Advent of Code 2020
 
 Solutions to the Advent of Code 2020 competition. No promises are made about
-whether or not these are *correct* solutions. 🙃
+whether or not these are *correct* or even *good* solutions. 🙃
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,41 +1,109 @@
+use std::error::Error;
+use std::fmt;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::{error::Error, path::PathBuf};
+use std::io::{self, BufRead, BufReader};
+use std::path::PathBuf;
+use std::str::FromStr;
 
-fn count_valid<'a, T: AsRef<str>>(lines: impl Iterator<Item = T>) -> usize {
-    lines
-        .into_iter()
-        .filter(|s| {
-            let parts = s
-                .as_ref()
-                .split(|c| match c {
-                    '-' | ' ' | ':' => true,
-                    _ => false,
-                })
-                .collect::<Vec<_>>();
-            let min = parts[0].parse::<usize>().unwrap();
-            let max = parts[1].parse::<usize>().unwrap();
-            let character = parts[2].chars().next().unwrap();
-            let pwd = parts[4];
-            let occurrence = pwd
-                .chars()
-                .filter(|&c| c == character)
-                .take(max + 1)
-                .count();
-            (min..=max).contains(&occurrence)
+#[derive(Debug)]
+enum Day2Error {
+    IoError(io::Error),
+    ParseError,
+}
+
+impl fmt::Display for Day2Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Day2Error::IoError(e) => write!(f, "IO error {}", e),
+            Day2Error::ParseError => write!(f, "Parse error"),
+        }
+    }
+}
+
+impl Error for Day2Error {}
+
+#[derive(Debug, PartialEq)]
+struct LineInfo {
+    pub min: usize,
+    pub max: usize,
+    pub character: char,
+    pub password: String,
+}
+
+fn iter_parse<'a, T: FromStr>(iter: &mut impl Iterator<Item = &'a str>) -> Result<T, Day2Error> {
+    iter.next()
+        .map(|s| s.parse::<T>().ok())
+        .flatten()
+        .ok_or(Day2Error::ParseError)
+}
+
+fn split_line(line: impl AsRef<str>) -> Result<LineInfo, Day2Error> {
+    let mut parts = line
+        .as_ref()
+        .split(|c| match c {
+            '-' | ' ' | ':' => true,
+            _ => false,
         })
-        .count()
+        .filter(|&s| !s.is_empty());
+    let min = iter_parse(&mut parts)?;
+    let max = iter_parse(&mut parts)?;
+    let character = iter_parse(&mut parts)?;
+    let password = parts.next().ok_or(Day2Error::ParseError)?.into();
+    match parts.next() {
+        None => Ok(LineInfo {
+            min,
+            max,
+            character,
+            password,
+        }),
+        _ => Err(Day2Error::ParseError),
+    }
+}
+
+fn count_valid<'a>(parsed_lines: impl Iterator<Item = &'a LineInfo>) -> Result<usize, Day2Error> {
+    Ok(parsed_lines
+        .filter(|&line_info| {
+            let occurrence = line_info
+                .password
+                .chars()
+                .filter(|&c| c == line_info.character)
+                .take(line_info.max + 1)
+                .count();
+            (line_info.min..=line_info.max).contains(&occurrence)
+        })
+        .count())
+}
+
+fn count_valid2<'a>(parsed_lines: impl Iterator<Item = &'a LineInfo>) -> Result<usize, Day2Error> {
+    Ok(parsed_lines
+        .filter(|&line_info| {
+            let mut password_chars = line_info.password.chars();
+            let first_ok = password_chars
+                .nth(line_info.min - 1)
+                .map_or(false, |c| c == line_info.character);
+            let second_ok = password_chars
+                .nth(line_info.max - line_info.min - 1)
+                .map_or(false, |c| c == line_info.character);
+            first_ok ^ second_ok
+        })
+        .count())
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
     let mut path = PathBuf::new();
     ["data", "day2", "input.txt"]
         .iter()
-        .for_each(|p| path.push(p));
+        .for_each(|&p| path.push(p));
     let file = File::open(path)?;
-    let lines = BufReader::new(file).lines().filter_map(Result::ok);
-    let valid_count = count_valid(lines);
-    println!("Found {} valid passwords", valid_count);
+    let parsed_lines = BufReader::new(file)
+        .lines()
+        .map(|l| l.map_err(Day2Error::IoError).and_then(split_line))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let part1 = count_valid(parsed_lines.iter())?;
+    println!("Part 1: found {} valid passwords", part1);
+    let part2 = count_valid2(parsed_lines.iter())?;
+    println!("Part 2: found {} valid passwords", part2);
     Ok(())
 }
 
@@ -51,12 +119,55 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use super::count_valid;
+    use super::{count_valid, count_valid2, split_line, LineInfo};
+    use std::error::Error;
+
+    const TEST_DATA: [&str; 3] = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+
+    fn create_test_info() -> Vec<LineInfo> {
+        vec![
+            LineInfo {
+                min: 1,
+                max: 3,
+                character: 'a',
+                password: String::from("abcde"),
+            },
+            LineInfo {
+                min: 1,
+                max: 3,
+                character: 'b',
+                password: String::from("cdefg"),
+            },
+            LineInfo {
+                min: 2,
+                max: 9,
+                character: 'c',
+                password: String::from("ccccccccc"),
+            },
+        ]
+    }
 
     #[test]
-    fn test1() {
-        let list = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
-        let count = count_valid(list.iter());
+    fn parse_test() -> Result<(), Box<dyn Error>> {
+        let parse_result = TEST_DATA
+            .iter()
+            .map(|&s| split_line(s))
+            .collect::<Result<Vec<_>, _>>()?;
+        assert!(parse_result.iter().eq(create_test_info().iter()));
+        Ok(())
+    }
+
+    #[test]
+    fn test1() -> Result<(), Box<dyn Error>> {
+        let count = count_valid(create_test_info().iter())?;
         assert_eq!(count, 2);
+        Ok(())
+    }
+
+    #[test]
+    fn test2() -> Result<(), Box<dyn Error>> {
+        let count = count_valid2(create_test_info().iter())?;
+        assert_eq!(count, 1);
+        Ok(())
     }
 }
