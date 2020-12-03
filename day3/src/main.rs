3e1947762643626f00f9bafe4690bcac7cb832c4
Andrew Tribick <ajtribick@googlemail.com>
2020-12-03 13:06:27 +0100

    Day 3, part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,16 +1,21 @@
 use std::io::{self, BufRead, BufReader};
 use std::{fs::File, path::PathBuf};
 
-const RIGHT_STEP: usize = 3;
+const SLOPES: [(usize, usize); 5] = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
-fn count_trees(lines: impl Iterator<Item = impl AsRef<str>>) -> u32 {
+fn count_trees(
+    lines: impl Iterator<Item = impl AsRef<str>>,
+    right_step: usize,
+    down_step: usize,
+) -> u32 {
     lines
+        .step_by(down_step)
         .fold((0, 0), |(mut trees, mut pos), line| {
             let line_ref = line.as_ref();
             if line_ref.chars().nth(pos).unwrap() == '#' {
                 trees += 1;
             }
-            pos += RIGHT_STEP;
+            pos += right_step;
             if pos >= line_ref.len() {
                 pos -= line_ref.len();
             }
@@ -19,23 +24,33 @@ fn count_trees(lines: impl Iterator<Item = impl AsRef<str>>) -> u32 {
         .0
 }
 
-fn part1() -> Result<(), io::Error> {
-    let mut path = PathBuf::new();
-    ["data", "day3", "input.txt"]
+fn part1(lines: impl Iterator<Item = impl AsRef<str>>) {
+    let trees = count_trees(lines, 3, 1);
+    println!("Part 1: encountered {} trees", trees);
+}
+
+fn part2(lines: &Vec<impl AsRef<str>>) {
+    let result = SLOPES
         .iter()
-        .for_each(|p| path.push(p));
+        .map(|&(right_step, down_step)| count_trees(lines.iter(), right_step, down_step))
+        .product::<u32>();
+    println!("Part 2: product is {}", result);
+}
+
+fn run() -> Result<(), io::Error> {
+    let path = ["data", "day3", "input.txt"].iter().collect::<PathBuf>();
     let input_file = File::open(path)?;
-    let lines = BufReader::new(input_file).lines().filter_map(Result::ok);
+    let lines = BufReader::new(input_file)
+        .lines()
+        .collect::<Result<Vec<_>, _>>()?;
 
-    let trees = count_trees(lines);
-
-    println!("Encountered {} trees", trees);
-
+    part1(lines.iter());
+    part2(&lines);
     Ok(())
 }
 
 fn main() {
-    std::process::exit(match part1() {
+    std::process::exit(match run() {
         Ok(_) => 0,
         Err(e) => {
             eprintln!("Error occurred: {}", e);
@@ -46,7 +61,7 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-    use super::count_trees;
+    use super::{count_trees, SLOPES};
 
     const EXAMPLE_LAYOUT: [&str; 11] = [
         "..##.......",
@@ -64,7 +79,23 @@ mod test {
 
     #[test]
     fn part1() {
-        let trees = count_trees(EXAMPLE_LAYOUT.iter());
+        let trees = count_trees(EXAMPLE_LAYOUT.iter(), 3, 1);
         assert_eq!(trees, 7);
+    }
+
+    #[test]
+    fn part2() {
+        let trees = SLOPES
+            .iter()
+            .map(|&(right_step, down_step)| {
+                count_trees(EXAMPLE_LAYOUT.iter(), right_step, down_step)
+            })
+            .collect::<Vec<_>>();
+
+        let expected = [2, 7, 3, 4, 2];
+
+        assert!(trees.iter().eq(expected.iter()));
+
+        assert_eq!(trees.iter().product::<u32>(), 336);
     }
 }
