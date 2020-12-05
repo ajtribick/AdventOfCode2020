aa8458065108d396d9d4b408d99f0ee669cf655b
Andrew Tribick <ajtribick@googlemail.com>
2020-12-05 10:21:45 +0100

    Day 5, part 1

---
+++ day5/src/main.rs
@@ -0,0 +1,81 @@
+use std::error::Error;
+use std::fmt;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::PathBuf;
+
+#[derive(Debug)]
+enum Day5Error {
+    NoData,
+}
+
+impl fmt::Display for Day5Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Day5Error::NoData => write!(f, "No data"),
+        }
+    }
+}
+
+impl Error for Day5Error {}
+
+fn calculate_id(pass: impl AsRef<str>) -> i32 {
+    pass.as_ref().chars().fold(0, |acc, c| {
+        (acc << 1)
+            + match c {
+                'B' | 'R' => 1,
+                _ => 0,
+            }
+    })
+}
+
+fn part1(lines: impl Iterator<Item = impl AsRef<str>>) -> Result<(), Day5Error> {
+    let max_value = lines.map(calculate_id).max().ok_or(Day5Error::NoData)?;
+    println!("Part 1: maximum ID = {}", max_value);
+    Ok(())
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let path = ["data", "day5", "input.txt"]
+        .iter()
+        .fold(PathBuf::new(), |mut acc, p| {
+            acc.push(p);
+            acc
+        });
+    let file = File::open(path)?;
+    let lines = BufReader::new(file)
+        .lines()
+        .collect::<Result<Vec<_>, _>>()?;
+    part1(lines.iter())?;
+    Ok(())
+}
+
+fn main() {
+    std::process::exit(match run() {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("Error occurred: {}", e);
+            1
+        }
+    });
+}
+
+#[cfg(test)]
+mod test {
+    use super::calculate_id;
+
+    const EXAMPLE_IDS: [(&str, i32); 4] = [
+        ("FBFBBFFRLR", 357),
+        ("BFFFBBFRRR", 567),
+        ("FFFBBBFRRR", 119),
+        ("BBFFBBFRLL", 820),
+    ];
+
+    #[test]
+    fn parse_fb() {
+        for &(pass, expected_id) in EXAMPLE_IDS.iter() {
+            let actual_id = calculate_id(pass);
+            assert_eq!(actual_id, expected_id);
+        }
+    }
+}
