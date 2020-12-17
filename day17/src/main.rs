b71e73c68336cca1e25e0168345efe0a85939359
Andrew Tribick <ajtribick@googlemail.com>
2020-12-17 22:17:41 +0100

    Day 17

---
+++ day17/src/main.rs
@@ -0,0 +1,31 @@
+use std::{error::Error, fs::read_to_string, path::PathBuf};
+
+mod simulation;
+use simulation::{ParseSimulationError, Simulation};
+
+fn process(initial: &str, dimensions: usize) -> Result<usize, ParseSimulationError> {
+    let mut simulation = Simulation::parse(initial, dimensions)?;
+    for _ in 0..6 {
+        simulation.update();
+    }
+
+    Ok(simulation.active_count())
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    let path = ["data", "day17", "input.txt"].iter().collect::<PathBuf>();
+    let initial = read_to_string(path)?;
+    println!("Part 1: result = {}", process(&initial, 3)?);
+    println!("Part 2: result = {}", process(&initial, 4)?);
+    Ok(())
+}
+
+fn main() {
+    std::process::exit(match run() {
+        Ok(_) => 0,
+        Err(e) => {
+            eprintln!("Error occurred: {}", e);
+            1
+        }
+    });
+}
