d49fcbc79b7ddc30261910e019dd13daafa061d8
Andrew Tribick <ajtribick@googlemail.com>
2020-12-20 21:08:35 +0100

    Day 20, part 2

--- day20/src/grid.rs
+++ day20/src/grid.rs
@@ -1,6 +1,9 @@
-use std::{cmp::Ordering, error::Error, fmt};
+use std::{error::Error, fmt};
 
-use crate::tile::{parse_tiles, ParseTileError};
+use crate::{
+    tile::{parse_tiles, EdgeConstraints, ParseTileError, Tile},
+    utils::sqrt_exact,
+};
 
 #[derive(Debug)]
 pub enum ParseGridError {
@@ -19,24 +22,74 @@ impl fmt::Display for ParseGridError {
 
 impl Error for ParseGridError {}
 
-fn grid_size(count: usize) -> Option<usize> {
-    for size in 0usize.. {
-        if let Some(sqr_size) = size.checked_mul(size) {
-            match sqr_size.cmp(&count) {
-                Ordering::Less => (),
-                Ordering::Equal => return Some(size),
-                Ordering::Greater => return None,
+fn find_corner(parsed_tiles: &mut [Tile]) -> Option<usize> {
+    let (corner, edges1, edges2) = parsed_tiles.iter().enumerate().find_map(|(idx, tile)| {
+        let mut connected = parsed_tiles
+            .iter()
+            .filter(|t| t.id() != tile.id())
+            .map(|t| tile.connect(t))
+            .filter(|c| !c.is_empty());
+        let first = connected.next()?;
+        let second = connected.next()?;
+        connected
+            .next()
+            .map_or_else(|| Some((idx, first, second)), |_| None)
+    })?;
+
+    let oriented = edges1
+        .iter()
+        .copied()
+        .flat_map(|e1| edges2.iter().copied().map(move |e2| (e1, e2)))
+        .any(|(e1, e2)| parsed_tiles[corner].orient(EdgeConstraints::right(e1).and_bottom(e2)));
+
+    if oriented {
+        Some(corner)
+    } else {
+        None
+    }
+}
+
+fn build_grid(
+    parsed_tiles: &mut Vec<Tile>,
+    corner: usize,
+    size: usize,
+) -> Result<Vec<Tile>, ParseGridError> {
+    let mut tiles = Vec::with_capacity(parsed_tiles.len());
+
+    tiles.push(parsed_tiles.remove(corner));
+
+    while !parsed_tiles.is_empty() {
+        let idx = tiles.len();
+        let mut constraints = EdgeConstraints::default();
+        if idx % size != 0 {
+            constraints.and_left(tiles[idx - 1].right_edge());
+        }
+
+        if idx >= size {
+            constraints.and_top(tiles[idx - size].bottom_edge());
+        }
+
+        let mut success = false;
+        for src_idx in 0..parsed_tiles.len() {
+            if parsed_tiles[src_idx].orient(&constraints) {
+                tiles.push(parsed_tiles.remove(src_idx));
+                success = true;
+                break;
             }
-        } else {
-            return None;
+        }
+
+        if !success {
+            return Err(ParseGridError::GridError("Ambiguous grid edge constraints"));
         }
     }
 
-    unreachable!()
+    Ok(tiles)
 }
 
 pub struct Grid {
-    corners: Vec<u64>,
+    size: usize,
+    tile_size: usize,
+    tiles: Vec<Tile>,
 }
 
 impl Grid {
@@ -45,36 +98,50 @@ impl Grid {
         S: AsRef<str>,
         I: Iterator<Item = S>,
     {
-        let tiles = parse_tiles(lines).map_err(ParseGridError::TileError)?;
-        let size = grid_size(tiles.len()).ok_or(ParseGridError::GridError("Non-square grid"))?;
+        let mut parsed_tiles = parse_tiles(lines).map_err(ParseGridError::TileError)?;
+        let size =
+            sqrt_exact(parsed_tiles.len()).ok_or(ParseGridError::GridError("Non-square grid"))?;
 
-        let mut corners = Vec::with_capacity(4);
-        let mut edges = 0;
-        for (id, tile) in tiles.iter() {
-            let connect_count = tiles
-                .iter()
-                .filter(|(other_id, other_tile)| *id != **other_id && tile.can_connect(other_tile))
-                .count();
-            match connect_count {
-                2 => corners.push(*id),
-                3 => edges += 1,
-                4 => (),
-                _ => return Err(ParseGridError::GridError("Ambiguous connections")),
+        let tile_size = parsed_tiles[0].size();
+
+        let corner = find_corner(&mut parsed_tiles).ok_or(ParseGridError::GridError(
+            "Could not find suitable top-left corner",
+        ))?;
+
+        let tiles = build_grid(&mut parsed_tiles, corner, size)?;
+
+        Ok(Self {
+            size,
+            tile_size,
+            tiles,
+        })
+    }
+
+    pub fn corner_ids(&self) -> [u64; 4] {
+        [
+            self.tiles[0].id(),
+            self.tiles[self.size - 1].id(),
+            self.tiles[self.tiles.len() - self.size].id(),
+            self.tiles[self.tiles.len() - 1].id(),
+        ]
+    }
+
+    pub fn merge_tiles(&self) -> Tile {
+        let inner_size = self.tile_size - 2;
+        let mut tile_data = Vec::with_capacity(self.size * self.size * inner_size * inner_size);
+        for grid_row in self.tiles.chunks(self.size) {
+            for row in 1..=inner_size {
+                for tile in grid_row.iter() {
+                    let inner_start = self.tile_size * row + 1;
+                    let inner_end = self.tile_size * (row + 1) - 1;
+                    for &element in tile.data()[inner_start..inner_end].iter() {
+                        tile_data.push(element);
+                    }
+                }
             }
         }
 
-        if corners.len() != 4 {
-            return Err(ParseGridError::GridError("Incorrect corner count"));
-        }
-        if edges != size.saturating_sub(2) * 4 {
-            return Err(ParseGridError::GridError("Incorrect edge count"));
-        }
-
-        Ok(Self { corners })
-    }
-
-    pub fn corner_ids(&self) -> &[u64] {
-        &self.corners
+        Tile::from_data(&tile_data, 0).unwrap()
     }
 }
 
@@ -90,5 +157,54 @@ mod test {
         let expected = [1951, 3079, 2971, 1171];
         assert_eq!(grid.corner_ids().len(), 4);
         assert!(grid.corner_ids().iter().all(|id| expected.contains(id)));
+    }
+
+    const EXAMPLE_MERGED: &str = r".#.#..#.##...#.##..#####
+###....#.#....#..#......
+##.##.###.#.#..######...
+###.#####...#.#####.#..#
+##.#....#.##.####...#.##
+...########.#....#####.#
+....#..#...##..#.#.###..
+.####...#..#.....#......
+#..#.##..#..###.#.##....
+#.####..#.####.#.#.###..
+###.#.#...#.######.#..##
+#.####....##..########.#
+##..##.#...#...#.#.#.#..
+...#..#..#.#.##..###.###
+.#.#....#.##.#...###.##.
+###.#...#..#.##.######..
+.#.#.###.##.##.#..#.##..
+.####.###.#...###.#..#.#
+..#.#..#..#.#.#.####.###
+#..####...#.#.#.###.###.
+#####..#####...###....##
+#.##..#..#...#..####...#
+.#.###..##..##..####.##.
+...###...##...#...#..###";
+
+    #[test]
+    fn merge_tiles_test() {
+        let grid = Grid::parse(EXAMPLE_DATA.lines()).unwrap();
+        let expected = EXAMPLE_MERGED
+            .lines()
+            .flat_map(|line| line.chars().map(|c| c == '#'))
+            .collect::<Vec<_>>();
+        let mut result = grid.merge_tiles();
+        let mut found_result = false;
+        for i in 0..8 {
+            if result.data() == expected {
+                found_result = true;
+                break;
+            }
+
+            result.rotate_right();
+            if i == 3 {
+                result.flip_horizontal();
+            }
+        }
+
+        assert!(found_result);
     }
 }
